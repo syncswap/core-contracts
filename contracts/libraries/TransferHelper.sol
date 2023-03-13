a192dedbf00b437e38b07a6918c1329e7c78e80d
nakato <94997114+0xnakato@users.noreply.github.com>
2023-03-13 15:36:58 +0000

    fix: revert assembly optimizations
    fix potential compatibility issues

--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -16,115 +16,55 @@ error ApproveFailed();
 
 /// @dev Helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true / false.
 library TransferHelper {
-    /// @dev Sets `amount` of ERC20 `token` for `to` to manage on behalf of the current contract.
-    /// Reverts upon failure.
-    function safeApprove(address token, address to, uint256 amount) internal {
-        /// @solidity memory-safe-assembly
-        assembly {
-            mstore(0x1a, to) // Store the `to` argument.
-            mstore(0x3a, amount) // Store the `amount` argument.
-            // Store the function selector of `approve(address,uint256)`,
-            // left by 6 bytes (enough for 8tb of memory represented by the free memory pointer).
-            // We waste 6-3 = 3 bytes to save on 6 runtime gas (PUSH1 0x224 SHL).
-            mstore(0x00, 0x095ea7b3000000000000)
+    function safeApprove(
+        address token,
+        address to,
+        uint value
+    ) internal {
+        // bytes4(keccak256(bytes("approve(address,uint256)")));
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
 
-            if iszero(
-                and( // The arguments of `and` are evaluated from right to left.
-                    // Set success to whether the call reverted, if not we check it either
-                    // returned exactly 1 (can't just be non-zero data), or had no return data.
-                    or(eq(mload(0x00), 1), iszero(returndatasize())),
-                    call(gas(), token, 0, 0x16, 0x44, 0x00, 0x20)
-                )
-            ) {
-                // Store the function selector of `ApproveFailed()`.
-                mstore(0x00, 0x3e3f8f73)
-                // Revert with (offset, size).
-                revert(0x1c, 0x04)
-            }
-            // Restore the part of the free memory pointer that was overwritten,
-            // which is guaranteed to be zero, if less than 8tb of memory is used.
-            mstore(0x3a, 0)
+        if (!success || (data.length != 0 && !abi.decode(data, (bool)))) {
+            revert ApproveFailed();
         }
     }
 
-    /// @dev Sends `amount` of ERC20 `token` from the current contract to `to`.
-    /// Reverts upon failure.
-    function safeTransfer(address token, address to, uint256 amount) internal {
-        /// @solidity memory-safe-assembly
-        assembly {
-            mstore(0x1a, to) // Store the `to` argument.
-            mstore(0x3a, amount) // Store the `amount` argument.
-            // Store the function selector of `transfer(address,uint256)`,
-            // left by 6 bytes (enough for 8tb of memory represented by the free memory pointer).
-            // We waste 6-3 = 3 bytes to save on 6 runtime gas (PUSH1 0x224 SHL).
-            mstore(0x00, 0xa9059cbb000000000000)
+    function safeTransfer(
+        address token,
+        address to,
+        uint value
+    ) internal {
+        // bytes4(keccak256(bytes("transfer(address,uint256)")));
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
 
-            if iszero(
-                and( // The arguments of `and` are evaluated from right to left.
-                    // Set success to whether the call reverted, if not we check it either
-                    // returned exactly 1 (can't just be non-zero data), or had no return data.
-                    or(eq(mload(0x00), 1), iszero(returndatasize())),
-                    call(gas(), token, 0, 0x16, 0x44, 0x00, 0x20)
-                )
-            ) {
-                // Store the function selector of `TransferFailed()`.
-                mstore(0x00, 0x90b8ec18)
-                // Revert with (offset, size).
-                revert(0x1c, 0x04)
-            }
-            // Restore the part of the free memory pointer that was overwritten,
-            // which is guaranteed to be zero, if less than 8tb of memory is used.
-            mstore(0x3a, 0)
+        if (!success || (data.length != 0 && !abi.decode(data, (bool)))) {
+            revert TransferFailed();
         }
     }
 
-    /// @dev Sends `amount` of ERC20 `token` from `from` to `to`.
-    /// Reverts upon failure.
-    ///
-    /// The `from` account must have at least `amount` approved for
-    /// the current contract to manage.
-    function safeTransferFrom(address token, address from, address to, uint256 amount) internal {
-        /// @solidity memory-safe-assembly
-        assembly {
-            let m := mload(0x40) // Cache the free memory pointer.
+    function safeTransferFrom(
+        address token,
+        address from,
+        address to,
+        uint value
+    ) internal {
+        // bytes4(keccak256(bytes("transferFrom(address,address,uint256)")));
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
 
-            // Store the function selector of `transferFrom(address,address,uint256)`.
-            mstore(0x00, 0x23b872dd)
-            mstore(0x20, from) // Store the `from` argument.
-            mstore(0x40, to) // Store the `to` argument.
-            mstore(0x60, amount) // Store the `amount` argument.
-
-            if iszero(
-                and( // The arguments of `and` are evaluated from right to left.
-                    // Set success to whether the call reverted, if not we check it either
-                    // returned exactly 1 (can't just be non-zero data), or had no return data.
-                    or(eq(mload(0x00), 1), iszero(returndatasize())),
-                    call(gas(), token, 0, 0x1c, 0x64, 0x00, 0x20)
-                )
-            ) {
-                // Store the function selector of `TransferFromFailed()`.
-                mstore(0x00, 0x7939f424)
-                // Revert with (offset, size).
-                revert(0x1c, 0x04)
-            }
-
-            mstore(0x60, 0) // Restore the zero slot to zero.
-            mstore(0x40, m) // Restore the free memory pointer.
+        if (!success || (data.length != 0 && !abi.decode(data, (bool)))) {
+            revert TransferFromFailed();
         }
     }
 
-    /// @dev Sends `amount` (in wei) ETH to `to`.
-    /// Reverts upon failure.
-    function safeTransferETH(address to, uint256 amount) internal {
-        /// @solidity memory-safe-assembly
-        assembly {
-            // Transfer the ETH and check if it succeeded or not.
-            if iszero(call(gas(), to, amount, 0, 0, 0, 0)) {
-                // Store the function selector of `ETHTransferFailed()`.
-                mstore(0x00, 0xb12d13eb)
-                // Revert with (offset, size).
-                revert(0x1c, 0x04)
-            }
+    function safeTransferETH(address to, uint value) internal {
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, ) = to.call{value: value}("");
+
+        if (!success) {
+            revert ETHTransferFailed();
         }
     }
 }
