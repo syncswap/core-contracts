6e4896abeddc230f7765ab44a060bdffa7070da0
nakato <94997114+0xnakato@users.noreply.github.com>
2023-03-15 20:09:56 +0000

    update: added IERC20PermitAllowed to SelfPermit

--- contracts/abstract/SelfPermit.sol
+++ contracts/abstract/SelfPermit.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.8.0;
 
 import "../interfaces/token/IERC20Permit2.sol";
+import "../interfaces/token/IERC20PermitAllowed.sol";
 
 abstract contract SelfPermit {
     function selfPermit(
@@ -26,6 +27,30 @@ abstract contract SelfPermit {
     ) external payable {
         if (IERC20(token).allowance(msg.sender, address(this)) < value) {
             selfPermit(token, value, deadline, v, r, s);
+        }
+    }
+
+    function selfPermitAllowed(
+        address token,
+        uint256 nonce,
+        uint256 expiry,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public payable {
+        IERC20PermitAllowed(token).permit(msg.sender, address(this), nonce, expiry, true, v, r, s);
+    }
+
+    function selfPermitAllowedIfNecessary(
+        address token,
+        uint256 nonce,
+        uint256 expiry,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external payable {
+        if (IERC20(token).allowance(msg.sender, address(this)) < type(uint256).max) {
+            selfPermitAllowed(token, nonce, expiry, v, r, s);
         }
     }
 
